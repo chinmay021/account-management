9158ccd63e74f64200b8b5ee94570d29e4049dad
chinmay021 <chinmaykumar021@gmail.com>
2020-11-01 09:00:32 +0530

    added font and fixed overflow issue

--- src/Login.js
+++ src/Login.js
@@ -2,19 +2,17 @@ import React from "react";
 import "./Login.css";
 function Login() {
   return (
-    <div>
-      <div className="newcontainer">
-        <div className="signup-box">
-          <div className="title">Enter Credentials</div>
-          <div className="textbox">
-            <input type="text" placeholder="Email" />
-          </div>
-          <div className="textbox">
-            <input type="password" placeholder="Password" />
-          </div>
-          <div className="button">
-            <input type="button" value="LOGIN" />
-          </div>
+    <div className="newcontainer">
+      <div className="signup-box">
+        <div className="title">Enter Credentials</div>
+        <div className="textbox">
+          <input type="text" placeholder="Email" />
+        </div>
+        <div className="textbox">
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="button">
+          <input type="button" value="LOGIN" />
         </div>
       </div>
     </div>
